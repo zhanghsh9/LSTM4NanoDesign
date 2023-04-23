35c339cd02007b879e8c037135778751967137c5
zhanghsh9 <zhanghsh9@gmail.com>
2023-04-23 18:40:31 +0800

    Update backward

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ import time
 import shutil
 
 from data import create_dataset
-from models import ForwardPredictionLSTM
-from train import train_epochs_forward
+from models import ForwardPredictionLSTM, BackwardPredictionLSTM
+from train import train_epochs_forward, train_epochs_backward
 from parameters import RESULTS_PATH, DATA_PATH, FIGS_PATH, MODEL_PATH, RODS, BATCH_SIZE, NUM_WORKERS, SAMPLE_RATE, \
     LEARNING_RATE, EPOCHS, NUM_LAYERS, ATTENTION, HIDDEN_UNITS, NUM_LSTMS, STEP_SIZE, GAMMA
 
@@ -62,6 +62,14 @@ if __name__ == '__main__':
     forward_test_dataloader = DataLoader(forward_test_dataset, batch_size=BATCH_SIZE, shuffle=False,
                                          num_workers=NUM_WORKERS, drop_last=True, pin_memory=True)
 
+    backward_train_dataset, backward_test_dataset = create_dataset(data_path=DATA_PATH, rods=RODS, reverse=True,
+                                                                   use_TL=True, transform=transform,
+                                                                   sample_rate=SAMPLE_RATE)
+    backward_train_dataloader = DataLoader(forward_train_dataset, batch_size=BATCH_SIZE, shuffle=True,
+                                           num_workers=NUM_WORKERS, drop_last=True, pin_memory=True)
+    backward_test_dataloader = DataLoader(forward_test_dataset, batch_size=BATCH_SIZE, shuffle=False,
+                                          num_workers=NUM_WORKERS, drop_last=True, pin_memory=True)
+
     print('{}: Using dataset:'.format(time.strftime("%Y%m%d  %H:%M:%S", time.localtime())))
     print()
     print('Train:')
@@ -73,7 +81,7 @@ if __name__ == '__main__':
     print('{}: Complete initializing dataset'.format(time.strftime("%Y%m%d  %H:%M:%S", time.localtime())))
     print()
 
-    for ATTENTION in [i/2. for i in range(13)]:
+    for ATTENTION in [i / 2. for i in range(13)]:
 
         print('ATTENTION = {}'.format(ATTENTION))
 
@@ -81,35 +89,36 @@ if __name__ == '__main__':
         input_len = forward_train_dataset.max_src_seq_len
         out_len = forward_train_dataset.max_tgt_seq_len
 
-        model = ForwardPredictionLSTM(attention=ATTENTION, input_len=input_len, hidden_units=HIDDEN_UNITS,
-                                      out_len=out_len, num_layers=NUM_LAYERS, num_lstms=NUM_LSTMS).to(device)
+        forward_model = ForwardPredictionLSTM(attention=ATTENTION, input_len=input_len, hidden_units=HIDDEN_UNITS,
+                                              out_len=out_len, num_layers=NUM_LAYERS, num_lstms=NUM_LSTMS).to(device)
 
-        for p in model.parameters():
+        for p in forward_model.parameters():
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
-        # This code is very important! It initialises the parameters with a range of values that stops the signal fading or
-        # getting too big.
-        # See https://andyljones.tumblr.com/post/110998971763/an-explanation-of-xavier-initialization
-        # for a mathematical explanation.
 
-        loss_fn_MSE = MSELoss().to(device)
-        forward_optimizer_Adam = Adam(model.parameters(), lr=LEARNING_RATE)
+        # This code is very important! It initialises the parameters with a range of values that stops the signal
+        # fading or getting too big.
+        # See https://andyljones.tumblr.com/post/110998971763/an-explanation-of-xavier-initialization for a mathematical
+        # explanation.
+
+        forward_loss_fn_MSE = MSELoss().to(device)
+        forward_optimizer_Adam = Adam(forward_model.parameters(), lr=LEARNING_RATE)
 
         # See https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.StepLR.html
-        step_lr = StepLR(forward_optimizer_Adam, step_size=STEP_SIZE, gamma=GAMMA, verbose=True)
+        forward_step_lr = StepLR(forward_optimizer_Adam, step_size=STEP_SIZE, gamma=GAMMA, verbose=True)
 
         # Train
-        model, x_axis_loss, x_axis_vloss, loss_record, vloss_record = train_epochs_forward(
-            training_loader=forward_train_dataloader, test_loader=forward_test_dataloader, model=model,
-            loss_fn=loss_fn_MSE, optimizer=forward_optimizer_Adam, scheduler=step_lr, attention=ATTENTION,
-            epochs=EPOCHS)
+        forward_model, x_axis_loss, x_axis_vloss, loss_record, vloss_record = train_epochs_forward(
+            training_loader=forward_train_dataloader, test_loader=forward_test_dataloader, model=forward_model,
+            loss_fn=forward_loss_fn_MSE, optimizer=forward_optimizer_Adam, scheduler=forward_step_lr,
+            attention=ATTENTION, epochs=EPOCHS)
 
         # Save model
         model_name = 'Forward_epochs_{}_lstms_{}_hidden_{}_attn_{}.pth'.format(EPOCHS, NUM_LSTMS, HIDDEN_UNITS,
                                                                                ATTENTION)
         if os.path.exists(os.path.join(model_save_path, model_name)):
             os.remove(os.path.join(model_save_path, model_name))
-        torch.save(model, os.path.join(model_save_path, model_name))
+        torch.save(forward_model, os.path.join(model_save_path, model_name))
 
         # Draw loss figure
         figs_name = 'loss_forward_attn_{}.png'.format(ATTENTION)
@@ -129,6 +138,50 @@ if __name__ == '__main__':
                      'BATCH_SIZE': BATCH_SIZE, 'NUM_LAYERS': NUM_LAYERS, 'NUM_LSTMS': NUM_LSTMS,
                      'LEARNING_RATE': LEARNING_RATE, 'STEP_SIZE': STEP_SIZE, 'GAMMA': GAMMA}
         scio.savemat(os.path.join(RESULTS_PATH, timestamp, 'loss_attn{}.mat'.format(ATTENTION)), mdict=loss_save)
+
+        end_time = time.time()
+        print()
+        print('{}: Total time used: {}'.format(time.strftime("%Y%m%d  %H:%M:%S", time.localtime()),
+                                               time.strftime('%H h %M m %S s ', time.gmtime(end_time - start_time))))
+
+        start_time = time.time()
+        # Backward
+        backward_model = BackwardPredictionLSTM(attention=ATTENTION, input_len=input_len, hidden_units=HIDDEN_UNITS,
+                                                out_len=out_len, num_layers=NUM_LAYERS, num_lstms=NUM_LSTMS).to(device)
+
+        for p in backward_model.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+
+        backward_loss_fn_MSE = MSELoss().to(device)
+        backward_optimizer_Adam = Adam(backward_model.parameters(), lr=LEARNING_RATE)
+
+        backward_step_lr = StepLR(backward_optimizer_Adam, step_size=STEP_SIZE, gamma=GAMMA, verbose=True)
+
+        forward_model, x_axis_loss, x_axis_vloss, loss_record, vloss_record = train_epochs_backward(
+            training_loader=backward_train_dataloader, test_loader=backward_test_dataloader,
+            backward_model=backward_model, loss_fn=backward_loss_fn_MSE, optimizer=backward_optimizer_Adam,
+            scheduler=backward_step_lr, attention=ATTENTION, epochs=EPOCHS)
+
+        model_name = 'Backward_epochs_{}_lstms_{}_hidden_{}_attn_{}.pth'.format(EPOCHS, NUM_LSTMS, HIDDEN_UNITS,
+                                                                                ATTENTION)
+        if os.path.exists(os.path.join(model_save_path, model_name)):
+            os.remove(os.path.join(model_save_path, model_name))
+        torch.save(forward_model, os.path.join(model_save_path, model_name))
+
+        # Draw loss figure
+        figs_name = 'loss_backward_attn_{}.png'.format(ATTENTION)
+        # plt.axes(yscale="log")
+        plt1, = plt.plot(x_axis_loss, loss_record, label='loss')
+        plt2, = plt.plot(x_axis_vloss, vloss_record, label='vloss')
+        plt.legend()
+        plt.xlabel('epoch')
+        plt.ylabel('Loss')
+        plt.title('Loss to epochs, backward')
+        if os.path.exists(os.path.join(figs_save_path, figs_name)):
+            os.remove(os.path.join(figs_save_path, figs_name))
+        plt.savefig(os.path.join(figs_save_path, figs_name))
+        plt.show()
 
         end_time = time.time()
         print()
--- train.py
+++ train.py
@@ -134,3 +134,139 @@ def train_epochs_forward(training_loader, test_loader, model, loss_fn, optimizer
         loss_record.append(float(avg_loss))
         x_axis_loss.append(epoch + 1)
     return model, x_axis_loss, x_axis_vloss, loss_record, vloss_record
+
+
+def train_one_epoch_backward(training_loader, optimizer, backward_model, forward_model, loss_fn):
+    """
+
+    :param training_loader:
+    :param optimizer:
+    :param backward_model:
+    :param forward_model:
+    :param loss_fn:
+    :return:
+    """
+
+    start_time = time.time()
+    running_loss = 0.
+    last_loss = 0.
+    epoch_loss = 0.
+
+    for i, data in enumerate(training_loader):
+        inputs, labels = data
+        inputs, labels = inputs.float().to(device), labels.float().to(device)
+
+        outputs, _ = backward_model(inputs)
+        outputs_forward, _ = forward_model(outputs)
+
+        optimizer.zero_grad()
+        loss = loss_fn(outputs_forward, inputs)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        epoch_loss += loss.item()
+        if i % 64 == 0:
+            if i == 0:
+                last_loss = running_loss
+            else:
+                last_loss = running_loss / 64
+
+            running_loss = 0.
+            print('{}: batch {} loss: {}'.format(time.strftime("%Y%m%d  %H:%M:%S", time.localtime()), i + 1, last_loss))
+
+    end_time = time.time()
+    print('{}: Time used: {}'.format(time.strftime("%Y%m%d  %H:%M:%S", time.localtime()),
+                                     time.strftime('%H h %M m %S s ', time.gmtime(end_time - start_time))))
+
+    return epoch_loss / (i + 1)
+
+
+def train_epochs_backward(training_loader, test_loader, backward_model, loss_fn, optimizer, scheduler, attention,
+                          epochs=EPOCHS):
+    """
+    Train backward model for epochs
+    :param backward_model: torch.nn.Module
+    :param attention: double
+    :param scheduler: torch.optim.lr_scheduler.StepLR
+    :param epochs: int
+    :param test_loader: torch.utils.data.DataLoader
+    :param training_loader: torch.utils.data.DataLoader
+    :param loss_fn: torch.nn.CrossEntropyLoss
+    :param optimizer: torch.optim.Adam
+    :return: model and loss
+    """
+
+    # Loss record
+    vloss_record = []
+    loss_record = []
+    x_axis_loss = []
+    x_axis_vloss = []
+
+    # Save model path
+    timestamp = datetime.now().strftime('%Y%m%d')
+    model_save_path = os.path.join(RESULTS_PATH, timestamp, MODEL_PATH)
+
+    # Load forward model
+    forward_model_name = 'Forward_mse_vloss_best_attn_{}.pth'.format(attention)
+    forward_model = torch.load(os.path.join(model_save_path, forward_model_name))
+    forward_model.train(True)
+    forward_model.to(device)
+    # Freeze parameters
+    for paras in forward_model.parameters():
+        paras.requires_grad = False
+
+    # Train
+    for epoch in range(epochs):
+        print('{}: Forward EPOCH {}:'.format(time.strftime("%Y%m%d  %H:%M:%S", time.localtime()), epoch + 1))
+        backward_model.train(True)
+        avg_loss = train_one_epoch_backward(training_loader=training_loader, backward_model=backward_model,
+                                            forward_model=forward_model, loss_fn=loss_fn, optimizer=optimizer)
+        scheduler.step()
+
+        # See https://discuss.pytorch.org/t/how-can-we-release-gpu-memory-cache/14530/5
+        if device == 'cuda':
+            torch.cuda.empty_cache()
+
+        # Eval
+        if epoch % VALID_FREQ == 0 or epoch + 1 == epochs:
+
+            # Evaluation
+            backward_model.eval()
+            running_vloss = 0.0
+            for i, vdata in enumerate(test_loader):
+                vinputs, vlabels = vdata
+                vinputs, vlabels = vinputs.float().to(device), vlabels.float().to(device)
+
+                voutputs, _ = backward_model(vinputs)
+                voutputs_forward, _ = backward_model(voutputs)
+
+                vloss = loss_fn(voutputs_forward, vinputs).item()
+                running_vloss += vloss
+
+            avg_vloss = running_vloss / (i + 1)
+            print('{}: LOSS train: {}, valid: {}'.format(time.strftime("%Y%m%d  %H:%M:%S", time.localtime()),
+                                                         avg_loss, avg_vloss))
+            vloss_record.append(float(avg_vloss))
+            x_axis_vloss.append(epoch + 1)
+
+            if epoch == 0:
+                best_vloss = avg_vloss
+
+            elif avg_vloss < best_vloss:
+                # Save model
+                model_name = 'Backward_mse_vloss_best_attn_{}.pth'.format(attention)
+                if os.path.exists(os.path.join(model_save_path, model_name)):
+                    os.remove(os.path.join(model_save_path, model_name))
+                torch.save(backward_model, os.path.join(model_save_path, model_name))
+                best_vloss = avg_vloss
+
+            vloss_record.append(float(avg_vloss))
+            x_axis_vloss.append(epoch + 1)
+        else:
+            print('{}: Training Loss:{}'.format(time.strftime("%Y%m%d  %H:%M:%S", time.localtime()), avg_loss))
+
+        print()
+        loss_record.append(float(avg_loss))
+        x_axis_loss.append(epoch + 1)
+    return backward_model, x_axis_loss, x_axis_vloss, loss_record, vloss_record
